38f65eab0c5f1bd1a9c545b29c90489d30786ef3
MinatoXeon <15091591733@126.com>
2022-11-10 22:46:45 +0800

    Finished the bolo.

--- bolo/bolo.cc
+++ bolo/bolo.cc
@@ -6,10 +6,13 @@
 #include <thread>
 #include <unordered_set>
 
+#include "compress.h"
 #include "copy.h"
+#include "crypto.h"
 #include "libfswatch/c++/libfswatch_exception.hpp"
 #include "libfswatch/c++/monitor.hpp"
 #include "libfswatch/c++/monitor_factory.hpp"
+#include "tar.h"
 #include "util.h"
 
 namespace bolo {
@@ -171,6 +174,54 @@ clean:
   return Err("filesystem error: "s + e.what());
 }
 
+// `f` has already being inserted into config
+Insidious<std::string> Bolo::BackupImpl(BackupFile &f, const std::string &key) {
+  auto temp = f.path;
+
+  if (f.is_compressed || f.is_encrypted) {
+    temp = MakeTemp();
+    if (auto tar = bolo_tar::Tar::Open(temp)) {
+      if (auto res = tar.value()->Append(f.path)) return Danger("tar error: "s + res.error());
+    } else {
+      return Danger(tar.error());
+    }
+
+    if (f.is_compressed) {
+      auto t = MakeTemp();
+      std::ifstream ifs(temp, std::ios_base::binary);
+      if (!ifs.good()) return Danger("failded to open "s + temp);
+      std::ofstream ofs(t, std::ios_base::binary | std::ios_base::trunc);
+      if (!ofs.good()) return Danger("failded to open "s + t);
+
+      if (auto ins = bolo_compress::Compress(ifs, ofs, bolo_compress::Scheme::DEFLATE))
+        return Danger("compression error: "s + ins.error());
+
+      // update temp
+      temp = t;
+    }
+
+    if (f.is_encrypted) {
+      if (key == "") return Danger("the file is encrypted, but the key is empty"s);
+
+      auto t = MakeTemp();
+      std::ifstream ifs(temp, std::ios_base::binary);
+      if (!ifs.good()) return Danger("failded to open "s + temp);
+      std::ofstream ofs(t, std::ios_base::binary | std::ios_base::trunc);
+      if (!ofs.good()) return Danger("failded to open "s + t);
+
+      if (auto ins = bolo_crypto::Encrypt(ifs, ofs, key))
+        return Danger("encrypt error: "s + ins.error());
+
+      // update temp
+      temp = t;
+    }
+  }
+
+  // remove the old file
+  // if (fs::exists(f.backup_path)) fs::remove_all(f.backup_path);
+
+  // rename temp
+  // cannot use rename: Invalid cross-device link
   std::thread([f, temp]() {
     try {
       bolo_copy::Copy copy;
@@ -179,8 +230,133 @@ clean:
       Log(LogLevel::Error, "copy error: "s + e.what());
     }
   }).detach();
+  // fs::copy(temp, f.backup_path, fs::copy_options::update_existing | fs::copy_options::recursive);
   return Safe;
 }
 
+// 删除一个备份文件
+Insidious<std::string> Bolo::Remove(BackupFileId id) try {
+  if (backup_files_.find(id) == backup_files_.end())
+    return Danger("No backup file with a BackupFileId of "s + std::to_string(id));
+
+  auto file = backup_files_[id];
+
+  fs::remove_all(file.backup_path);
+
+  backup_files_.erase(id);
+
+  return UpdateConfig();
+} catch (const fs::filesystem_error &e) {
+  return Danger("filesystem error: "s + e.what());
+}
+
+// 更新一个备份文件
+Insidious<std::string> Bolo::Update(BackupFileId id, const std::string &key) try {
+  if (backup_files_.find(id) == backup_files_.end())
+    return Danger("No backup file with a BackupFileId of "s + std::to_string(id));
+
+  BackupFile &file = backup_files_[id];
+
+  if (file.is_encrypted && key == "") return Danger("the file is encrypted, but the key is empty"s);
+
+  if (auto ins = BackupImpl(file, key)) return ins;
+
+  if (auto ins = UpdateConfig()) return ins;
+
+  // update timestamp
+  file.timestamp = GetTimestamp();
+  return Safe;
+} catch (const fs::filesystem_error &e) {
+  return Danger("filesystem error: "s + e.what());
+}
+
+Insidious<std::string> Bolo::Restore(BackupFileId id, const fs::path &restore_dir,
+                                     const std::string &key) try {
+  using bolo_tar::Tar;
+
+  // check if the restore dir exists
+  if (!fs::exists(restore_dir)) return Danger("directory does not exist: "s + restore_dir.string());
+  if (!fs::is_directory(restore_dir))
+    return Danger("the restore_path should be a directory: "s + restore_dir.string());
+
+  // check if the file id is right
+  if (backup_files_.find(id) == backup_files_.end())
+    return Danger("No backup file with a BackupFileId of "s + std::to_string(id));
+  const BackupFile &file = backup_files_[id];
+
+  // check if the backup file exists
+  if (!fs::exists(file.backup_path))
+    return Danger("backup file does not exist: "s + file.backup_path);
+
+  // check is there is a conflict file in the restore_dir
+  auto restore_path = restore_dir / file.filename;
+  if (fs::exists(restore_path))
+    return Danger("file `"s + file.filename + "` already exists in `" + restore_dir.string() + "`");
+
+  if (file.is_encrypted && key == "") return Danger("the file is encrypted, but the key is empty"s);
+
+  std::string temp = file.backup_path;
+
+  if (file.is_encrypted) {
+    auto t = MakeTemp();
+
+    std::ifstream ifs(temp, std::ios_base::binary);
+    if (!ifs.good()) return Danger("failded to open "s + file.backup_path);
+    std::ofstream ofs(t, std::ios_base::binary | std::ios_base::trunc);
+    if (!ofs.good()) return Danger("failded to open "s + temp);
+
+    if (auto ins = bolo_crypto::Decrypt(ifs, ofs, key))
+      return Danger("decrypto error: "s + ins.error());
+
+    temp = t;
+  }
+
+  if (file.is_compressed) {
+    auto t = MakeTemp();
+
+    std::ifstream ifs(temp, std::ios_base::binary);
+    if (!ifs.good()) return Danger("failded to open "s + file.backup_path);
+    std::ofstream ofs(t, std::ios_base::binary | std::ios_base::trunc);
+    if (!ofs.good()) return Danger("failded to open "s + temp);
+
+    if (auto ins = bolo_compress::Uncompress(ifs, ofs, bolo_compress::Scheme::DEFLATE))
+      return Danger("compression error: "s + ins.error());
+
+    temp = t;
+  }
+
+  if (file.is_compressed || file.is_encrypted) {
+    if (auto tar = Tar::Open(temp)) {
+      if (auto res = tar.value()->Extract(restore_dir)) return Danger("tar error: "s + res.error());
+    } else {
+      return Danger("tar error: "s + tar.error());
+    }
+  } else {
+    bolo_copy::Copy copy;
+    auto res = copy.FullCopy(temp, restore_path);
+  }
+  return Safe;
+} catch (const fs::filesystem_error &e) {
+  return Danger("filesystem error: "s + e.what());
+}
+
+Insidious<std::string> Bolo::UpdateConfig() {
+  try {
+    config_["next_id"] = next_id_;
+    config_["backup_list"] = json(backup_files_);
+  } catch (const json::type_error &e) {
+    return Danger("json type_error: "s + e.what());
+  }
+
+  if (enable_auto_update_)
+    thread_ = std::make_shared<std::thread>([this](auto t) { this->UpdateMonitor(t); }, thread_);
+
+  std::ofstream f(config_file_path_);
+  if (!f.is_open()) return Danger("failed to open config file: "s + config_file_path_.string());
+
+  f << config_.dump(4);
+  if (!f.good()) return Danger("failed to write to config file: "s + config_file_path_.string());
+  return Safe;
+}
 };  // namespace bolo
 
